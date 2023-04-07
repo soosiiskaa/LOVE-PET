962e4c15fe3e2c8ce94c6e0a2edc9d2c240fb529
soosiiskaa <120337946+soosiiskaa@users.noreply.github.com>
2023-04-07 07:05:19 +0300

    LOVE&PET

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOVE&PET</title>
+    <style>
+        body{
+            background-color: #1E1E1E;
+            background-size: cover;
+            width: 100%;
+        }
+
+        li {
+    list-style-type: none; 
+   }
+    </style>
+
+<link rel="stylesheet" href="./css/logo.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<div>
+    <center><img  src="./img/лого.png" alt="//" class="catlogo"></div></center>
+
+<div class="txt"><p  align="center">LOVE&PET - интернет-магазин товаров для питомцев</p></div>
+<div class="avtor">
+    <p align="center">
+        Иванцова Кристина Кирилловна группа 609-21
+    </p>
+</div>
+</body>
+</html>
